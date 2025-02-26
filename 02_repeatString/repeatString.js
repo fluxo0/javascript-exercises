d6b5d0366e7ee5f3e28400b71d2b1881395ce183
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-02-26 17:38:51 -0300

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = "";
 
+    if (num >= 0) {
+        for (let i = 1; i <= num; i++) {
+            finalString += string;
+        } 
+    } else {
+        return 'ERROR';
+    }
+    
+    return finalString;
 };
 
 // Do not edit below this line
