30751468e413a19f1d7248219d298427eedeeeb2
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-02-26 21:08:42 -0300

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, ...theArgs) {
+    const result = [];
+
+    nextNum:
+    for (const number of numbers) {
+        for (const arg of theArgs) {
+            if (number === arg) continue nextNum;
+        }
+
+        result.push(number);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
