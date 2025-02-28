e565458831be0437c4236511130cb8e9ade2c2ca
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-02-28 15:31:26 -0300

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const numbers = [];
+    let sum = 0;
 
+    if (num1 < 0 || num2 < 0 ||
+        !Number.isInteger(num1) || !Number.isInteger(num2) 
+    ) return "ERROR";
+
+    if (num1 > num2) {
+        for (let i = num1; i >= num2; i--) {
+            numbers.push(i);
+        }
+    } else {
+        for (let i = num1; i <= num2; i++) {
+            numbers.push(i);
+        }
+    }
+
+    for (const number of numbers) {
+        sum += number;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
