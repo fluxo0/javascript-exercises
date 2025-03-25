53863bcec5c00d5ee9a886477a7c9b1b72f936b6
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-03-25 01:50:29 -0300

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, value) => acc + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, value) => acc * value, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  // return Math.pow(a, b);
+
+  let result = 1;
+	for (let i = y; i > 0; i--) {
+    result *= x;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) 1;
+
+  let arr = [];
+  for (let i = num; i > 0; i--) {
+    arr.push(i);
+  }
+
+  return arr.reduce((acc, value) => acc * value, 1);
 };
 
 // Do not edit below this line
