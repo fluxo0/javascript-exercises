349e1991718c40f1376a485beb1eae7811ce5057
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-03-26 03:36:51 -0300

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const regex = /[A-Z0-9]/g;
+    const strUpcased = str.toUpperCase();
+    let arr = strUpcased.match(regex);
+    let arrReversed = arr.toReversed();
 
+    str = arr.join("");
+    const strReversed = arrReversed.join("");
+
+    return str === strReversed;  
 };
 
 // Do not edit below this line
