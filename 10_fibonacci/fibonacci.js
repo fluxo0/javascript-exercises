23c283e4b3879ebcb6ef18528b6c05e56c022311
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-03-26 19:35:48 -0300

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    if (num === 1) return 1;
 
+    let firstNum = 0;
+    let secondNum = 1;
+    let acc = 0;
+    for (let i = 2; i <= num; i++) {
+        acc = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = acc;
+    }
+    return acc;
 };
 
 // Do not edit below this line
