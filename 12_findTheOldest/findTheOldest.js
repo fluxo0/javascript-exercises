aa8430ded78f0053009c46b52be9cfdf564fe3bc
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-03-27 04:12:48 -0300

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  arr.sort((a, b) => {
+    let ageA, ageB;
 
+    ageA = checkAge(a);
+    ageB = checkAge(b);
+
+    return ageB - ageA;
+  });
+
+  return arr[0];
 };
+
+function checkAge(obj) {
+  let age;
+
+  let year  = new Date();
+  year = year.getFullYear();
+
+  if (Object.hasOwn(obj, "yearOfDeath")) {
+    age = obj.yearOfDeath - obj.yearOfBirth;
+  } else {
+    age = year - obj.yearOfBirth;
+  }
+
+  return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
